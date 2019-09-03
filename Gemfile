91a77565669ba99cf4243b9c70707e1060122c8f
Sufl0wer <fr08ty@gmail.com>
2019-09-03 15:59:27 +0300

    fix getting json response

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
+ruby '2.6.3'
+
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem 'mechanize'
-gem 'nokogiri'
 gem 'pry'
+gem 'rb-readline'
+gem 'rest-client'
--- schedule_parser.rb
+++ schedule_parser.rb
@@ -1,9 +1,7 @@
-require 'json'
-
 class ScheduleParser
   STUDENT_SCHEDULE_LINK = 'https://journal.bsuir.by/api/v1/studentGroup/schedule?studentGroup='
 
   def self.group_schedule(group_number)
-    JSON.parse(STUDENT_SCHEDULE_LINK + group_number.to_s)
+    JSON.parse RestClient.get(STUDENT_SCHEDULE_LINK + group_number)
   end
 end
